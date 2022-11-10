fd701bae73d450cb28d090e71cb4a03ec43de347
Oskar Dragon <herrschade@192.168.1.152>
2022-11-10 18:24:35 +0000

    turn off linting rules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import { trpc } from '../utils/trpc';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
 export default trpc.withTRPC(App);
-
-
