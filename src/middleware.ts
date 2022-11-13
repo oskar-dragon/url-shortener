4d40e2cfc9404d3f94c1b5f1344ae8034843b803
Oskar Dragon <oskartdragon@gmail.com>
2022-11-13 20:37:00 +0000

    feat: add redirect

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export const config = {
+  matcher: ['/((?!api|_next/static|favicon.ico).*)'],
+};
+
+export async function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname.startsWith('/api/') || req.nextUrl.pathname === '/') {
+    return;
+  }
+  const slug = req.nextUrl.pathname.split('/').pop();
+
+  const fetchSlug = await fetch(`${req.nextUrl.origin}/api/get-link/${slug}`);
+
+  if (fetchSlug.status === 404) {
+    return NextResponse.redirect(req.nextUrl.origin);
+  }
+
+  const data = await fetchSlug.json();
+
+  if (data?.longUrl) {
+    return NextResponse.redirect(data.longUrl);
+  }
+}
---
+++ src/pages/api/get-link/[slug].ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from 'server/prisma';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query.slug;
+
+  if (!slug || typeof slug !== 'string') {
+    res.status(404).json({ message: 'please provide a correct slug' });
+
+    return;
+  }
+
+  const data = await prisma.url.findFirst({
+    where: {
+      shortUrl: {
+        equals: slug,
+      },
+    },
+  });
+
+  if (!data) {
+    res.status(404).json({ message: 'short link not found' });
+    return;
+  }
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 's-maxage=1000000000, stale-while-revalidate');
+
+  res.json(data);
+};
