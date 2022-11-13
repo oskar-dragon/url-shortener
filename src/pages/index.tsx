9443c209113bddfe030ae7154cf828fbb4acbf64
Oskar Dragon <herrschade@192.168.1.152>
2022-11-13 18:08:28 +0000

    feat: refactor styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,31 +18,51 @@ const defaultValues: FormFields = {
 
 export default function Home() {
   return (
-    <div className="max-w-xl mx-auto bg-base-200 rounded-md py-4 px-6 flex flex-col">
-      <h1 className="text-white text-5xl font-bold text-center">Link shortener</h1>
-
-      <Form<FormFields>
-        defaultValues={defaultValues}
-        onSubmit={() => console.log('submitted')}
-        schema={formData}
-      >
-        <div className="flex flex-col gap-4">
-          <FormInput
-            name="longurl"
-            id="longurl"
-            label="Long url"
-            placeholder="Enter your long url"
-          />
-          <FormInput
-            name="shorturl"
-            id="shorturl"
-            label="Alias"
-            placeholder="enter your alias"
-            className="max-w-xs"
-          />
-          <Button type="submit">Submit</Button>
+    <div className="hero max-w-xl lg:max-w-4xl mx-auto bg-base-200 rounded-md shadow-xl py-6 px-8">
+      <div className="hero-content flex-col lg:flex-row-reverse ">
+        <div className="text-center lg:text-left lg:pl-8">
+          <h1 className="text-white text-5xl font-bold">Link shortener</h1>
+          <p className="py-2">
+            Provide the following details to make your link shorter and easier to remember
+          </p>
+          <div className="hidden lg:block space-y-3 py-9">
+            <p className="font-bold text-white">Create free account and enjoy:</p>
+            <ul className="list-none">
+              <li>Link history</li>
+              <li>Customized TinyURLs</li>
+            </ul>
+            <Button className="btn-secondary">Sign up for free!</Button>
+          </div>
         </div>
-      </Form>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <Form<FormFields>
+              defaultValues={defaultValues}
+              onSubmit={() => console.log('submitted')}
+              schema={formData}
+            >
+              <div className="flex flex-col gap-4">
+                <FormInput
+                  name="longurl"
+                  id="longurl"
+                  label="Long url"
+                  placeholder="Enter your long url"
+                />
+                <FormInput
+                  name="shorturl"
+                  id="shorturl"
+                  label="Alias"
+                  placeholder="enter your alias"
+                  className="max-w-xs"
+                />
+                <Button className="mt-4" type="submit">
+                  Make your link shorter
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
