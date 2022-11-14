9c7fc8bb69453d18c6adf1458f3df2b814da378f
Oskar Dragon <oskartdragon@gmail.com>
2022-11-14 21:43:35 +0000

    ..

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,7 +1,5 @@
-
-import * as trpc from '@trpc/server';
-import * as trpcNext from '@trpc/server/adapters/next';
-
+/* eslint-disable @typescript-eslint/return-await */
+import type * as trpc from '@trpc/server';
 
 interface CreateContextOptions {
   // session: Session | null
@@ -21,9 +19,7 @@ export type Context = trpc.inferAsyncReturnType<typeof createContextInner>;
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export async function createContext(
-  opts: trpcNext.CreateNextContextOptions,
-): Promise<Context> {
+export async function createContext(): Promise<Context> {
   // for API-response caching see https://trpc.io/docs/caching
 
   return await createContextInner({});
