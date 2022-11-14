b0494b3e014d50afb10b9ef4abe5d4069d5e2755
Oskar Dragon <oskartdragon@gmail.com>
2022-11-14 21:29:58 +0000

    fix: remove unused import

--- src/features/shortener/index.ts
+++ src/features/shortener/index.ts
@@ -1,3 +1,3 @@
 export { shortenerValidation } from './types/shortenerForm';
-export type { FormSchemaType, FormFields } from './types/shortenerForm';
+export type { FormSchemaType } from './types/shortenerForm';
 export { default as generateSlug } from './helpers/generateSlug/generateSlug';
