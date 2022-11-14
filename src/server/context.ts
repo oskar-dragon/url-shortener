021b10593484bdded4627c1ab56ac1b8b298e3e8
Oskar Dragon <oskartdragon@gmail.com>
2022-11-14 21:45:08 +0000

    ..

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,15 +1,11 @@
 /* eslint-disable @typescript-eslint/return-await */
 import type * as trpc from '@trpc/server';
 
-interface CreateContextOptions {
-  // session: Session | null
-}
-
 /**
  * Inner function for `createContext` where we create the context.
  * This is useful for testing when we don't want to mock Next.js' request/response
  */
-export async function createContextInner(_opts: CreateContextOptions) {
+export async function createContextInner() {
   return {};
 }
 
@@ -22,5 +18,5 @@ export type Context = trpc.inferAsyncReturnType<typeof createContextInner>;
 export async function createContext(): Promise<Context> {
   // for API-response caching see https://trpc.io/docs/caching
 
-  return await createContextInner({});
+  return await createContextInner();
 }
