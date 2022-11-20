324d120371dda0bb198cd7c7c31d02079b6d605e
Oskar Dragon <oskartdragon@gmail.com>
2022-11-20 18:12:44 +0000

    fix: remove uneccessary link

--- src/components/layout/Avatar.tsx
+++ src/components/layout/Avatar.tsx
@@ -33,19 +33,6 @@ function Avatar() {
           <Menu.Item>
             {({ active }) => (
               <Link
-                href="/"
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'block px-4 py-2 text-sm text-gray-700',
-                )}
-              >
-                Your Profile
-              </Link>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <Link
                 href="/profile"
                 className={classNames(
                   active ? 'bg-gray-100' : '',
