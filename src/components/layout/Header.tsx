87ed8f5862773f6a8823386668ebafbf5406a808
Oskar Dragon <oskartdragon@gmail.com>
2022-11-22 21:07:33 +0000

    hide dashboard

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,11 +2,8 @@ import { Disclosure } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Private } from 'components';
 import Link from 'next/link';
-import classNames from '../../utils/classNames';
 import Avatar from './Avatar';
 import Navigation from './Navigation';
-
-const navigation = [{ name: 'Dashboard', href: '/dashboard', current: true }];
 
 function Header() {
   return (
@@ -54,22 +51,7 @@ function Header() {
 
             <Disclosure.Panel className="sm:hidden">
               <div className="space-y-1 px-2 pt-2 pb-3">
-                {navigation.map((item) => (
-                  <Disclosure.Button
-                    key={item.name}
-                    as="a"
-                    href={item.href}
-                    className={classNames(
-                      item.current
-                        ? 'bg-gray-900 text-white'
-                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'block px-3 py-2 rounded-md text-base font-medium',
-                    )}
-                    aria-current={item.current ? 'page' : undefined}
-                  >
-                    {item.name}
-                  </Disclosure.Button>
-                ))}
+                <Navigation />
               </div>
             </Disclosure.Panel>
           </>
--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,10 +1,11 @@
+import Private from 'components/private/Private';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { classNames } from 'utils';
 
 const navigation = [
-  { name: 'Home', href: '/' },
-  { name: 'Dashboard', href: '/dashboard' },
+  { name: 'Home', href: '/', private: false },
+  { name: 'Dashboard', href: '/dashboard', private: true },
 ];
 
 function Navigation() {
@@ -12,20 +13,44 @@ function Navigation() {
 
   return (
     <>
-      {navigation.map((item) => (
-        <Link
-          key={item.name}
-          href={item.href}
-          className={classNames(
-            router.asPath === item.href
-              ? 'bg-gray-900 text-white'
-              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-            'px-3 py-2 rounded-md text-sm font-medium',
-          )}
-        >
-          {item.name}
-        </Link>
-      ))}
+      {navigation.map((item) => {
+        if (item.private) {
+          return (
+            <Private
+              key={item.name}
+              privateComponent={
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    router.asPath === item.href
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-sm font-medium',
+                  )}
+                >
+                  {item.name}
+                </Link>
+              }
+            />
+          );
+        }
+
+        return (
+          <Link
+            key={item.name}
+            href={item.href}
+            className={classNames(
+              router.asPath === item.href
+                ? 'bg-gray-900 text-white'
+                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+              'block px-3 py-2 rounded-md text-sm font-medium',
+            )}
+          >
+            {item.name}
+          </Link>
+        );
+      })}
     </>
   );
 }
