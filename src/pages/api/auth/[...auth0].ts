87c6a43420d1f1d3e8a6f20ac8377b7980bf5180
Oskar Dragon <oskartdragon@gmail.com>
2022-11-22 20:26:23 +0000

    feat: save user in a db upon signup/login

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,3 +1,60 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
+import { Prisma } from '@prisma/client';
+import { prisma } from 'server/prisma';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import logger from 'server/utils/logger';
 
-export default handleAuth();
+type SessionType = {
+  user: {
+    email: string;
+    name: string;
+    updated_at: string;
+    [K: string]: any;
+  };
+};
+
+const afterCallback = async (req: NextApiRequest, res: NextApiResponse, session: SessionType) => {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { email, name, updated_at } = session.user;
+
+  if (email && typeof email === 'string') {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        await prisma.user.create({
+          data: { email, name, updatedAt: updated_at },
+        });
+      }
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        // The .code property can be accessed in a type-safe manner
+        if (error.code === 'P2002') {
+          logger.info(
+            'There is a unique constraint violation, a new user cannot be created with this email',
+          );
+        }
+      }
+    }
+  }
+
+  logger.info(`User with email: ${email} has been created successfully!`);
+
+  return session;
+};
+
+export default handleAuth({
+  async callback(req, res) {
+    try {
+      await handleCallback(req, res, { afterCallback });
+    } catch (error) {
+      if (error instanceof Error) {
+        logger.error(error.message);
+      }
+    }
+  },
+});
