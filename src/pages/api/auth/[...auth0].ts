e9f17a2f54fbf704393506c1b5004563127f308a
Oskar Dragon <oskartdragon@gmail.com>
2022-11-22 20:38:56 +0000

    ..

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,19 +1,11 @@
+import type { Session } from '@auth0/nextjs-auth0';
 import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
 import { Prisma } from '@prisma/client';
 import { prisma } from 'server/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import logger from 'server/utils/logger';
 
-type SessionType = {
-  user: {
-    email: string;
-    name: string;
-    updated_at: string;
-    [K: string]: any;
-  };
-};
-
-const afterCallback = async (req: NextApiRequest, res: NextApiResponse, session: SessionType) => {
+const afterCallback = async (req: NextApiRequest, res: NextApiResponse, session: Session) => {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   const { email, name, updated_at } = session.user;
 
@@ -32,7 +24,6 @@ const afterCallback = async (req: NextApiRequest, res: NextApiResponse, session:
       }
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        // The .code property can be accessed in a type-safe manner
         if (error.code === 'P2002') {
           logger.info(
             'There is a unique constraint violation, a new user cannot be created with this email',
