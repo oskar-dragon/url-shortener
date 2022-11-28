d56044cc9011f8d6a0acb9aa69e5a8c848739dc9
Oskar Dragon <oskartdragon@gmail.com>
2022-11-28 21:39:55 +0000

    fix: errors

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,7 +1,7 @@
 import * as trpcNext from '@trpc/server/adapters/next';
-import { appRouter } from '../../../server/routes/_app';
 import { createContext } from 'server/trpc';
 import logger from 'server/utils/logger';
+import { appRouter } from '../../../server/routes/_app';
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
--- src/server/routes/_app.ts
+++ src/server/routes/_app.ts
@@ -1,27 +1,8 @@
-import { z } from 'zod';
-import { publicProcedure, privateProcedure, router } from '../trpc';
-import { prisma } from '../prisma';
+import { router } from '../trpc';
 import { shortLinkRouter } from './shortLink';
 
 // TODO: Add routes here
 export const appRouter = router({
-  hello: publicProcedure
-    .input(
-      z.object({
-        text: z.string().nullish(),
-      }),
-    )
-    .query(async ({ input }) => {
-      const test = await prisma.url.findMany();
-
-      return {
-        greeting: `hello ${input?.text ?? 'world'}`,
-        urls: test,
-      };
-    }),
-  private: privateProcedure.query(() => {
-    return 'works';
-  }),
   shortLink: shortLinkRouter,
 });
 
