541a9b9263bff21642a48420e646ff48adb03aaa
Oskar Dragon <oskartdragon@gmail.com>
2023-01-05 15:44:49 +0000

    feat: add creatable select

--- src/components/searchDropdown/SearchDropdown.tsx
+++ src/components/searchDropdown/SearchDropdown.tsx
@@ -1,4 +1,5 @@
 import Select, { components } from 'react-select';
+import CreatableSelect from 'react-select/creatable';
 import type { GroupBase, Props } from 'react-select';
 import { cx } from 'class-variance-authority';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
@@ -17,10 +18,33 @@ function SearchDropdown<
   Option,
   IsMulti extends boolean = false,
   Group extends GroupBase<Option> = GroupBase<Option>,
->(props: Props<Option, IsMulti, Group>) {
-  return (
+>({ isClearable, ...restProps }: Props<Option, IsMulti, Group>) {
+  return isClearable ? (
+    <CreatableSelect
+      {...restProps}
+      components={{ Control }}
+      unstyled
+      classNames={{
+        container: ({ isFocused }) =>
+          cx(
+            'border rounded-lg border-neutral-300 bg-neutral-100 text-md py-0.5',
+            isFocused ? 'custom-focus' : '',
+          ),
+        control: () => 'pl-2.5 pr-2 placeholder:text-neutral-500',
+        placeholder: () => 'text-neutral-500',
+        valueContainer: ({ isDisabled }) =>
+          cx(isDisabled ? 'text-neutral-500 cursor-not-allowed' : 'text-black'),
+        indicatorsContainer: () => cx('w-0'),
+        dropdownIndicator: () => cx('text-transparent'),
+        menu: () => 'mt-2 rounded-lg border border-neutral-100 shadow-xl',
+        option: ({ isSelected }) =>
+          cx('py-2.5 px-3 custom-transition', isSelected ? 'bg-neutral-50' : 'hover:bg-neutral-50'),
+        noOptionsMessage: () => cx('py-2.5 px-3'),
+      }}
+    />
+  ) : (
     <Select
-      {...props}
+      {...restProps}
       components={{ Control }}
       unstyled
       classNames={{
