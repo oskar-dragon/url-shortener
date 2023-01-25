b59dad8a3b389991dee542ccaee8572db56f18ac
Oskar <oskartdragon@gmail.com>
2023-01-25 13:56:35 +0000

    fix: remove unused function

--- src/helpers/getRowId/getRowId.ts
+++
@@ -1,7 +0,0 @@
-import type { Row } from '@tanstack/react-table';
-
-function getRowId<TData extends any>(row: TData, index: number, parent?: Row<TData>): string {
-  return parent ? [parent.id, row?.uniqueId].join('.') : row.uniqueid;
-}
-
-export default getRowId;
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,2 @@
 export { default as copyToClipboard } from './copyToClipboard';
 export { default as getRandomValue } from './getRandomValue/getRandomValue';
-export { default as getRowId } from './getRowId/getRowId';
